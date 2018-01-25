61436c97001cb40ea661690f35c645b17369a218
dzuka <milandzukic@gmail.com>
2018-01-25 20:08:42 +0100

    Dodavanje Movies component i naziva u navbar-brand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './component/layout/layout.component';
+import { MoviesComponent } from './component/movies/movies.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
