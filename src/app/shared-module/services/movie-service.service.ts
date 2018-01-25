127a5bc232894d0c8030d4a99dd2e7d672b5c12c
dzuka <milandzukic@gmail.com>
2018-01-25 21:44:51 +0100

    nedovrseno

--- src/app/shared-module/services/movie-service.service.ts
+++ src/app/shared-module/services/movie-service.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { movies } from '../examples';
+import { Observable, Observer } from 'rxjs';
+import { Movie } from './models/movie';
 
 @Injectable()
 export class MovieServiceService {
 
-  constructor() { }
+  private movieList: Array<Movie>;
+
+  constructor() {
+    this.movieList = movies.map(this.mapMovies);
+  }
+
+  getMovies(){
+    return new Observable ((o: Observer<any>)=>{
+      o.next(this.movieList);
+      return o.complete();
+    })
+  }
 
 }
+
+private mapMovies(movie) {
+  return new Movie(
+    movie.id,
+    movie.name,
+    movie.director,
+    movie.imageUrl,
+    movie.duration,
+    movie.releaseDate,
+    movie.genres
+    )
+  }
+
