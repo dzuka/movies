9d72993e2db7a85c1840e4b1cfec87244f80047d
dzuka <milandzukic@gmail.com>
2018-01-25 21:30:33 +0100

    Dodavanje Examples fajla sa listom podataka o filmovima

---
+++ src/app/shared-module/examples.ts
@@ -0,0 +1,29 @@
+export const movies = [
+    {
+        id:1.
+        name: 'Movie 1',
+        director: 'Director 1',
+        imageUrl: 'http//img1.com',
+        duration: 94,
+        releaseDate: '01-01-01',
+        genres: ['action', 'drama']
+    },
+    {
+        id: 2,
+        name: 'Movie 2',
+        director: 'Director 2',
+        imageUrl: 'http//img2.com',
+        duration: 106,
+        releaseDate: '02-02-02',
+        genres: ['comedy', 'drama']
+    },
+    {
+        id: 3,
+        name: 'Movie 3',
+        director: 'Director 3',
+        imageUrl: 'http//img3.com',
+        duration: 121,
+        releaseDate: '03-03-03',
+        genres: ['comedy', 'action']   
+    }
+]
